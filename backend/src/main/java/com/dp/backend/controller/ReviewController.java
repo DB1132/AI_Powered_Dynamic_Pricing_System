280577d2686fe39b2ae91542e8326e958255ab31
DB1132 <baldhadeep5@gmail.com>
2026-07-01 14:53:28 +0530

    write code of get reviews by producut id

--- backend/src/main/java/com/dp/backend/controller/ReviewController.java
+++ backend/src/main/java/com/dp/backend/controller/ReviewController.java
@@ -1,4 +1,21 @@
 package com.dp.backend.controller;
 
+import com.dp.backend.model.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+@CrossOrigin
 public class ReviewController {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @GetMapping("/{id}")
+    public List<Review> getReviewsByProduct(@PathVariable Long id) {
+        return reviewRepository.findByProductId(id);
+    }
 }
--- backend/src/main/java/com/dp/backend/repository/ReviewRepository.java
+++ backend/src/main/java/com/dp/backend/repository/ReviewRepository.java
@@ -1,4 +1,12 @@
 package com.dp.backend.repository;
 
-public class ReviewRepository {
+import com.dp.backend.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByProductId(int productId);
 }
