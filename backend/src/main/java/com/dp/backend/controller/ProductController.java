bd4e4e94da6bc9dac37fdbff055325b9e7e67f36
DB1132 <baldhadeep5@gmail.com>
2026-07-01 10:56:16 +0530

    add logic of product controller(getproducts and addproducts)

--- backend/src/main/java/com/dp/backend/controller/ProductController.java
+++ backend/src/main/java/com/dp/backend/controller/ProductController.java
@@ -1,4 +1,31 @@
 package com.dp.backend.controller;
 
+import com.dp.backend.model.Product;
+import com.dp.backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    ProductRepository  productRepository;
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id){
+        return productRepository.findById(id);
+    }
 }
